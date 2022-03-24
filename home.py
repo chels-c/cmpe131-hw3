e42432dfa5cae7d097db018da45ce96f676ad29d
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-23 23:34:25 -0700

    Add files via upload

---
+++ home.py
@@ -0,0 +1,23 @@
+from flask import flask, url_for
+global name
+global city_names
+
+myobj = Flask(_name_)
+
+@myobj.route("/home")
+def home():
+	<html>
+	<head>
+	</head>
+	<body>
+		<h1>Welcome '''  + name '''</h1>
+		<a href="www.google.com">not google </a>
+		<ul>
+		{% for city.name in city_names %}
+       			<li>city</li>
+  		{% endfor %}
+		</ul>
+	</body>
+	</html>
+
+#myapp_obj.run()
