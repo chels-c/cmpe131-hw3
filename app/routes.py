e94c15f33a312d5ce43349ccf88aafaaf5bd2722
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-28 20:39:03 -0700

    Delete app directory

--- app/routes.py
+++
@@ -1,9 +0,0 @@
-from app import myobj
-from flask import render_template, flash
-
-name = ''
-city_names = ["city1", "city2"]
-
-@myobj.route('/')
-def home():
-	return render_template("home.html", name = name, city_names = city_names)
