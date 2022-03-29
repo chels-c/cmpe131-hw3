510fe2203d4fc1bf3e51c8ae286fd984b8851090
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-28 20:42:27 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ city_names = ["city1", "city2"]
 def home():
 	return render_template("home.html", name = name,city_names = city_names)
 
-@myobj.route('/submit')
+@myobj.route('/submit', methods = ['GET', 'POST'])
 def submit():
 	form = SubmitForm()
 	if form.validate_on_submit():
