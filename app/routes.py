7cac974cae8d8865698a2cf6290200eb3b2c83b0
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-28 22:29:08 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,19 @@ from flask import render_template, flash
 name = 'chels'
 city_names = ["city1", "city2"]
 
-@myobj.route('/')
+@myobj.route('/', methods = ['GET', 'POST'])
 def home():
-	return render_template("home.html", name = name,city_names = city_names)
-
+	form = SubmitForm()
+	if form.validate_on_submit():
+		flash(form.CityName.data)
+	return render_template("home.html", name = name,city_names = city_names, form=form)
+"""
 @myobj.route('/submit', methods = ['GET', 'POST'])
+
 def submit():
 	form = SubmitForm()
 	if form.validate_on_submit():
 		flash('submitted'.format(form.CityName.data))
 		return redirect('/')
 	return render_template('home.html', title = 'submit', form = form)
+"""
