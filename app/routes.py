0498f3c2748d507781314dd2ee88c7096c519210
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-28 20:39:35 -0700

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from app import myobj
+from flask import render_template, flash
+
+name = 'chels'
+city_names = ["city1", "city2"]
+
+@myobj.route('/')
+def home():
+	return render_template("home.html", name = name,city_names = city_names)
+
+@myobj.route('/submit')
+def submit():
+	form = SubmitForm()
+	if form.validate_on_submit():
+		flash('submitted'.format(form.CityName.data))
+		return redirect('/')
+	return render_template('submit.html', title = 'submit', form = form)
