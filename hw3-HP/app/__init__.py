8b599481b04284e8305adbf711f236d739ab4f15
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-28 21:05:43 -0700

    Delete hw3-HP directory

--- hw3-HP/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-myobj = Flask(__name__)
-
-from app import routes
--- hw3-HP/app/routes.py
+++
@@ -1,9 +0,0 @@
-from app import myobj
-from flask import render_templates, flash
-
-global name
-global city_names
-
-@myobj.route('/')
-def home():
-	return return_templates(home.html, name = name)
